6c6baf88d9f0602fdfd16515adb649f1de1d8431
Cameron Keif <keifcame@msu.edu>
2013-02-13 14:44:36 -0500

    Added edittext box for the hint in drawing activity. Portrait layout complete. Will work on landscape later.
    Creating guessing activity (blank right now) and added all my drawing
    stuff into a bundle to communicate with it (hint, score, etc.)

--- Scribbles/src/edu/msu/cse/monopoly/scribbles/DrawActivity.java
+++ Scribbles/src/edu/msu/cse/monopoly/scribbles/DrawActivity.java
@@ -1,11 +1,15 @@
 package edu.msu.cse.monopoly.scribbles;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -17,6 +21,8 @@ public class DrawActivity extends Activity {
     private static final String PLAYER2 = "player2";
     private static final String PLAYER1SCORE = "player1Score";
     private static final String PLAYER2SCORE = "player2Score";
+    private static final String HINT = "hint";
+    private static final String TOPIC = "topic";
     
     /**
      * Request code when selecting a color
@@ -28,24 +34,37 @@ public class DrawActivity extends Activity {
      */
     private Spinner spinner;
     
+    private ArrayList<String> possibleTopics = new ArrayList<String>();
+    private int player1Score;
+    private int player2Score;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_draw);
 		
+		possibleTopics.add(getString(R.string.animal));
+		possibleTopics.add(getString(R.string.building));
+		possibleTopics.add(getString(R.string.object));
+		possibleTopics.add(getString(R.string.action));
+		possibleTopics.add(getString(R.string.msu));
+		
         Bundle bundle = getIntent().getExtras();
 
         // Displays player names and their scores
         // bundle.getInt returns 0 by default, so coming from the welcome screen is not a problem
+        
+        player1Score = bundle.getInt(PLAYER1SCORE);
+        player2Score = bundle.getInt(PLAYER2SCORE);
         if(bundle.getString(PLAYER1)!= null)
         {
             TextView player1ScoreText = (TextView) findViewById(R.id.player1ScoreText);
             
             if (bundle.getString(PLAYER1).equals("")){ // User enters nothing
-            	player1ScoreText.setText(getString(R.string.player1) + ": " + Integer.toString(bundle.getInt(PLAYER1SCORE)));
+            	player1ScoreText.setText(getString(R.string.player1) + ": " + Integer.toString(player1Score));
             }
             else{
-            player1ScoreText.setText(bundle.getString(PLAYER1)+ ": " + Integer.toString(bundle.getInt(PLAYER1SCORE)));
+            player1ScoreText.setText(bundle.getString(PLAYER1)+ ": " + Integer.toString(player2Score));
             }
         }
         
@@ -61,6 +80,10 @@ public class DrawActivity extends Activity {
             }
         }
 
+        // Pick a random topic
+        Random rand = new Random();
+        TextView topicText = (TextView) findViewById(R.id.topicText);
+        topicText.setText(getString(R.string.topic) + " " + possibleTopics.get(rand.nextInt(possibleTopics.size())));
         
         spinner = (Spinner) findViewById(R.id.spinnerThickness);
         
@@ -95,8 +118,25 @@ public class DrawActivity extends Activity {
     	// Start the guessing activity
     	// Change DrawActivity.class to the guess activity class when it is created.
     	
-		/*Intent intent = new Intent(this, DrawActivity.class);
-		startActivity(intent);*/
+		Intent intent = new Intent(this, GuessActivity.class);
+		
+		// Put the artist's hint in the bundle
+		EditText hintBox = (EditText) findViewById(R.id.hintBox);
+		String hint = hintBox.getText().toString();
+		intent.putExtra(HINT, hint);
+		
+		// Put the player's scores in the bundle
+		intent.putExtra(PLAYER1SCORE, player1Score);
+		intent.putExtra(PLAYER2SCORE, player2Score);
+		
+		// Put the topic string into the bundle
+		TextView topic = (TextView) findViewById(R.id.topicText);
+		intent.putExtra(TOPIC, topic.getText().toString());
+		
+		/* NEED TO PUT THE ARRAY OF LINES IN DRAWINGVIEW INTO THE BUNDLE! */
+		
+		// Start the guessing activity
+		startActivity(intent);
     	
     }
     
