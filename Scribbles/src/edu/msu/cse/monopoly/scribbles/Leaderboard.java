7ad942f6f941145b7928012a391e35113357e33c
Cameron Keif <keifcame@msu.edu>
2013-04-14 20:18:30 -0400

    Removed Lint warning in Leaderboard

--- Scribbles/src/edu/msu/cse/monopoly/scribbles/Leaderboard.java
+++ Scribbles/src/edu/msu/cse/monopoly/scribbles/Leaderboard.java
@@ -10,7 +10,6 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.util.Xml;
 import android.view.View;
 import android.widget.TextView;
